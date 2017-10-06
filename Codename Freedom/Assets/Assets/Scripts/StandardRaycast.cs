628422ae8ccf071603bd006ebe58d5fa2a99c018
AaronMacaulay <vidjunkie@yahoo.ca>
2017-10-06 06:06:08 -0400

    First Milestone
    -Basic AI
    -Basic Level
    -Simple weapons
    -Menu functionality

---
+++ Codename Freedom/Assets/Assets/Scripts/StandardRaycast.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public static class StandardRaycast {
+	public static string GetTagFromRaycast(Vector3 origin, Vector3 direction, float length)
+	{
+		string retVal = "";
+		RaycastHit hitInfo;
+		if(Physics.Raycast (origin, direction, out hitInfo, length)){
+			retVal = hitInfo.collider.tag;
+		}
+		return retVal;
+	}
+
+}
