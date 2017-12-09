6be5e8d63a828815f4135fc4fc6fdc150518b1e3
AaronMacaulay <vidjunkie@yahoo.ca>
2017-12-09 17:19:39 -0500

    Camera fixes and project clean up

---
+++ Codename Freedom/Assets/Assets/Scripts/DestroyAircraft.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAircraft : MonoBehaviour {
+
+	public GameObject explosion;
+
+	void OnTriggerEnter(Collider impactObject)
+	{
+		if(impactObject.tag != "ChaseSensor" && impactObject.tag != "EnemyAI") {
+			Instantiate (explosion, transform.position, transform.rotation);
+			Destroy(transform.parent.parent.gameObject);
+		}
+	}
+}
